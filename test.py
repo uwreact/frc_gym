de5e16e5064bc994f64679cdea750982a4371c35
Ivan Zdane <izdane@gmail.com>
2019-05-18 10:33:55 -0700

    Add assertation testing

--- test.py
+++ test.py
@@ -13,49 +13,35 @@ protoEnv = gym.make("Proto-v1")
 #   3 = Cargo Ship
 
 #We start at the Hab, move to player station to pick up cargo
-print(protoEnv.step(1))
-#([10, True], 0, False)
+assert((protoEnv.step(1)) == ([10, True], 0, False))
 # ([currentTime, Carying Cargo], reward, done)
 
 # Move to Score at the rocket ship
-print(protoEnv.step(2))
-# ([25, False], 3, False)
+assert((protoEnv.step(2)) == ([25, False], 3, False))
 
 # Go Back to Player Station
-print(protoEnv.step(1))
-# ([40, True], 0, False)
+assert((protoEnv.step(1)) == ([40, True], 0, False))
 
 # Go to Cargo Ship
-print(protoEnv.step(3))
-# ([60, False], 2, False)
+assert((protoEnv.step(3)) == ([60, False], 2, False))
 
 # Go to Rocket
-print(protoEnv.step(2))
-# ([65, False], 0, False)
+assert((protoEnv.step(2)) == ([65, False], 0, False))
 
 #Go to HAB
-print(protoEnv.step(0))
-# ([85, False], 0, False)
+assert((protoEnv.step(0)) == ([85, False], 0, False))
 
 # Go To Player Station
-print(protoEnv.step(1))
-# ([95, False], 0, False)
+assert((protoEnv.step(1)) == ([95, True], 0, False))
 
 #Go to HAB
-print(protoEnv.step(0))
-# ([105, False], 0, False)
+assert((protoEnv.step(0)) == ([105, True], 0, False))
 
 # Go to Cargo Ship
-print(protoEnv.step(3))
-# ([120, False], 2, False)
+assert((protoEnv.step(3)) == ([120, False], 2, False))
 
 # Go To Player Station
-print(protoEnv.step(1))
-# ([140, False], 0, False)
+assert((protoEnv.step(1)) == ([140,  True], 0, False))
 
 # Go To Rocket 
-print(protoEnv.step(2))
-# (([150, True], 0, True)
-
-
-
+assert((protoEnv.step(2)) == ([150, True], 0, True))
