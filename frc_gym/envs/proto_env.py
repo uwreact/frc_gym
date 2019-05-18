5ad3bdb6f155dc4a15df67ccb804437ca4ea111d
Ivan Zdane <izdane@gmail.com>
2019-05-18 10:45:36 -0700

    Refactoring

--- frc_gym/envs/proto_env.py
+++ frc_gym/envs/proto_env.py
@@ -3,132 +3,123 @@ import numpy as np
 from gym import spaces, logger
 from gym.utils import seeding
 
+
 class ProtoEnv(gym.Env):
-	metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human']}
 
-	def __init__(self):
-		# Prototype environment for the 2019 FRC DeepSpace Game
-		# - Consists of only a HAB, Player Station, Rocket and Cago Ship
-		# - Agent begins at HAB, can pick up cargo at Player Station and then score
-		#   on either the Rocket or Cago Ship
-		# - Rocket awards 3 points, cargo ship awards 2 points
-		# - Epsiode ends once time runs out
+    def __init__(self):
+        # Prototype environment for the 2019 FRC DeepSpace Game
+        # - Consists of only a HAB, Player Station, Rocket and Cago Ship
+        # - Agent begins at HAB, can pick up cargo at Player Station and then score
+        #   on either the Rocket or Cago Ship
+        # - Rocket awards 3 points, cargo ship awards 2 points
+        # - Epsiode ends once time runs out
 
-		# Timing:
-		# HAB -> Player Station = 10
-		# HAB -> Rocket = 20
-		# HAB -> Cargo Ship = 15
-		# HAB -> HAB = 0
-		# PlayerStation -> Player Station = 0
-		# PlayerStation -> Rocket = 15
-		# PlayerStation -> Cargo Ship = 20
-		# PlayerStation -> HAB = 10
-		# Rocket -> Player Station = 15
-		# Rocket -> Rocket = 0
-		# Rocket -> Cargo Ship = 5
-		# Rocket -> HAB = 20
-		# Cargo Ship -> Player Station = 20
-		# Cargo Ship -> Rocket = 5
-		# Cargo Ship -> Cargo Ship = 0
-		# Cargo Ship -> HAB = 15
+        # Timing:
+        # HAB -> Player Station = 10
+        # HAB -> Rocket = 20
+        # HAB -> Cargo Ship = 15
+        # HAB -> HAB = 0
+        # PlayerStation -> Player Station = 0
+        # PlayerStation -> Rocket = 15
+        # PlayerStation -> Cargo Ship = 20
+        # PlayerStation -> HAB = 10
+        # Rocket -> Player Station = 15
+        # Rocket -> Rocket = 0
+        # Rocket -> Cargo Ship = 5
+        # Rocket -> HAB = 20
+        # Cargo Ship -> Player Station = 20
+        # Cargo Ship -> Rocket = 5
+        # Cargo Ship -> Cargo Ship = 0
+        # Cargo Ship -> HAB = 15
 
-		self.times = {
-			"HAB": {
-				"Player Station": 10,
-				"Rocket": 20,
-				"Cargo Ship": 15,
-				"HAB": 0
-			},
-			"Player Station": {
-				"Player Station": 0,
-				"Rocket": 15,
-				"Cargo Ship": 20,
-				"HAB": 10
-			},
-			"Rocket": {
-				"Player Station": 15,
-				"Rocket": 0,
-				"Cargo Ship": 5,
-				"HAB": 20
-			},
-			"Cargo Ship": {
-				"Player Station": 20,
-				"Rocket": 5,
-				"Cargo Ship": 0,
-				"HAB": 15
-			}
-		}
+        # Action Space: locations on field where agent can move:
+        #   0 = HAB
+        #   1 = Player Station
+        #   2 = Rocket
+        #   3 = Cargo Ship
+        self.action_space = spaces.Discrete(4)
 
-		self.time = 0
-		self.currentSate = 0
-		self.hasCargo = 0
-		self.done = False
+        # Observation Space:
+        # [time, carryingCargo]
+        self.observation_space = spaces.MultiDiscrete([150, 1])
 
-		# Action Space: locations on field where agent can move:
-		#   0 = HAB
-		#   1 = Player Station
-		#   2 = Rocket
-		#   3 = Cargo Ship
-		self.actionToName = {
-			0: "HAB",
-			1: "Player Station",
-			2: "Rocket",
-			3: "Cargo Ship"
-		}
+        #Amount of time to get from one location to another
+        self.times = {
+            0: {
+                1: 10,
+                2: 20,
+                3: 15,
+                0: 0
+            },
+            1: {
+                1: 0,
+                2: 15,
+                3: 20,
+                0: 10
+            },
+            2: {
+                1: 15,
+                2: 0,
+                3: 5,
+                0: 20
+            },
+            3: {
+                1: 20,
+                2: 5,
+                3: 0,
+                0: 15
+            }
+        }
 
-		self.action_space = spaces.Discrete(4)
+        self.time = 0
+        self.currentSate = 0
+        self.hasCargo = 0
+        self.done = False
 
-		# Observation Space: 
-		# [time, carryingCargo]
-		self.observation_space = spaces.MultiDiscrete([150, 1])
-		
-	def step(self, action):
-		reward = 0
+    def step(self, action):
+        reward = 0
 
-		self.time += self.times[self.actionToName[self.currentSate]][self.actionToName[action]]
-		self.currentSate = action
-		action = self.actionToName[action]
+        self.time += self.times[self.currentSate][action]
+        self.currentSate = action
 
+        if (self.time >= 150):
+            #Episode over
+            self.done = True
 
-		if (self.time >= 150):
-			#Episode over
-			self.done = True
+            self.time = 150
 
-			self.time = 150
+        if not self.done:
 
-		if not self.done:
-			
-			
-			
-			if action == "HAB":
-				reward = 0
-			if action == "Player Station":
-				self.hasCargo = True
-			if action == "Rocket":
-				if self.hasCargo:
-					reward = 3
-					self.hasCargo = False
-				else:
-					reward = 0
-			if action == "Cargo Ship":
-				if self.hasCargo:
-					reward = 2
-					self.hasCargo = False
-				else:
-					reward = 0
+            if action == 0:
+                reward = 0
+            if action == 1:
+                self.hasCargo = True
+            if action == 2:
+                if self.hasCargo:
+                    reward = 3
+                    self.hasCargo = False
+                else:
+                    reward = 0
+            if action == 3:
+                if self.hasCargo:
+                    reward = 2
+                    self.hasCargo = False
+                else:
+                    reward = 0
 
-		self.observation_space = [self.time, self.hasCargo]
-		return self.observation_space, reward, self.done
-		
-		
-	def reset(self):
-		self.time = 0
-		self.currentSate = 0
-		self.hasCargo = False
-		self.done = False
-		
-	def render(self, mode='human'):
-		# Implement a simple graph like using networkx
-		raise NotImplementedError
-	def close(self):
-		...
+        self.observation_space = [self.time, self.hasCargo]
+        return self.observation_space, reward, self.done
+
+    def reset(self):
+        self.time = 0
+        self.currentSate = 0
+        self.hasCargo = False
+        self.done = False
+
+    def render(self, mode='human'):
+        # Implement a simple graph like using networkx
+        raise NotImplementedError
+
+    def close(self):
+        ...
--- test.py
+++ test.py
@@ -5,7 +5,6 @@ env = gym.make('FrcGraph2019-v1')
 env2 = gym.make('Banana-v1')
 protoEnv = gym.make("Proto-v1")
 
-
 # ProtoEnv Test
 #   0 = HAB
 #   1 = Player Station
@@ -13,35 +12,35 @@ protoEnv = gym.make("Proto-v1")
 #   3 = Cargo Ship
 
 #We start at the Hab, move to player station to pick up cargo
-assert((protoEnv.step(1)) == ([10, True], 0, False))
+assert ((protoEnv.step(1)) == ([10, True], 0, False))
 # ([currentTime, Carying Cargo], reward, done)
 
 # Move to Score at the rocket ship
-assert((protoEnv.step(2)) == ([25, False], 3, False))
+assert ((protoEnv.step(2)) == ([25, False], 3, False))
 
 # Go Back to Player Station
-assert((protoEnv.step(1)) == ([40, True], 0, False))
+assert ((protoEnv.step(1)) == ([40, True], 0, False))
 
 # Go to Cargo Ship
-assert((protoEnv.step(3)) == ([60, False], 2, False))
+assert ((protoEnv.step(3)) == ([60, False], 2, False))
 
 # Go to Rocket
-assert((protoEnv.step(2)) == ([65, False], 0, False))
+assert ((protoEnv.step(2)) == ([65, False], 0, False))
 
 #Go to HAB
-assert((protoEnv.step(0)) == ([85, False], 0, False))
+assert ((protoEnv.step(0)) == ([85, False], 0, False))
 
 # Go To Player Station
-assert((protoEnv.step(1)) == ([95, True], 0, False))
+assert ((protoEnv.step(1)) == ([95, True], 0, False))
 
 #Go to HAB
-assert((protoEnv.step(0)) == ([105, True], 0, False))
+assert ((protoEnv.step(0)) == ([105, True], 0, False))
 
 # Go to Cargo Ship
-assert((protoEnv.step(3)) == ([120, False], 2, False))
+assert ((protoEnv.step(3)) == ([120, False], 2, False))
 
 # Go To Player Station
-assert((protoEnv.step(1)) == ([140,  True], 0, False))
+assert ((protoEnv.step(1)) == ([140, True], 0, False))
 
-# Go To Rocket 
-assert((protoEnv.step(2)) == ([150, True], 0, True))
+# Go To Rocket
+assert ((protoEnv.step(2)) == ([150, True], 0, True))
