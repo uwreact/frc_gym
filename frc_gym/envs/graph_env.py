59d26bfa49cf64a1a4b72a7c33d78175be734e7e
Ivan Zdane <izdane@gmail.com>
2019-06-05 21:23:30 -0700

    Update read me

--- frc_gym/envs/graph_env.py
+++ frc_gym/envs/graph_env.py
@@ -6,13 +6,25 @@ class GraphEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        ...
+        self.hasCargo = False
+        self.hasFuel = False
+        self.time = 0
+        self.done = False
+
+        # Observation Space:
+        # [time, carryingCargo, caryingFueld]
+        self.observation_space = spaces.MultiDiscrete([150, 1, 1])
+
+
 
     def step(self, action):
         ...
 
     def reset(self):
-        ...
+        self.hasCargo = False
+        self.hasFuel = False
+        self.time = 0
+        self.done = False
 
     def render(self, mode='human'):
         ...
